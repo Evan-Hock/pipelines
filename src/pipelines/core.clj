c198171ddfbc14524022466ea0a674d322d8dbc5
Evan Hock <hocke@oregonstate.edu>
2024-12-07 23:33:06 -0800

    Made underscore substitution slightly more efficient at runtime by introducing a let-binding for each substitution

--- src/pipelines/core.clj
+++ src/pipelines/core.clj
@@ -10,18 +10,20 @@
 (defn- underscore? [symbol]
   (= '_ symbol))
 
-(defmacro |>
+(defmacro ^:export |>
   "Pipeline operator.
    
   Functions like the standard \"thread last\" operator, but with some extensions:
   * If a form is a vector, it is expected to have two members, and the first member
     should be either `:first`, `:last`, or a number greater than or equal to 1. This will determine the piping mode,
-    that is, which argument into a form the preceding form should be inserted into.
+    that is, which argument into a form the preceding form should be inserted into. If you want to pipe a
+    value into a vector, you should put the vector into a list by surrounding it in parentheses.
   * If a form is a list, the preceding form will be inserted as the last element,
     unless that form contains underscores. If that form contains underscores,
     they will be substituted with the preceding form.
   * If a form is not a list, it will be converted into a list, and the preceding form will
     be appended as the last element (this is the same behaviour as the `->>` macro)." 
+  {:clj-kondo/ignore [:unresolved-symbol :invalid-arity]}
   [x & forms]
   (loop [x x, forms forms]
     (if (empty? forms)
@@ -37,12 +39,14 @@
                     :first (thread-nth 1 x form)
                     :last (thread-last x form)
                     (do
-                      (assert (and (integer? threading-mode) (>= threading-mode 1)) "Threading mode must be an integer and >= 1.")
+                      (assert (and (integer? threading-mode) (>= threading-mode 1)) "Threading mode must be an integer and greater than or equal to 1.")
                       (thread-nth threading-mode x form)))))
 
               (seq? form)
               (if (boolean (some underscore? form))
-                (map #(if (underscore? %) x %) form)
+                (let [g (gensym)]
+                  `(let [~g ~x]
+                     ~(map #(if (underscore? %) g %) form)))
                 (thread-last x form))
 
               :else (list form x))]
